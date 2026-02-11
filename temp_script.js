e0d6622428aa9c76327da82bacb828da8dc761d2
sajithps421-ops <sajithps421@gmail.com>
2026-02-11 20:05:24 +0530

    Update temp_script.js

--- temp_script.js
+++ temp_script.js
@@ -1,6 +1,20 @@
-alert("EXTERNAL JS LOADED");
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Exam</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="temp_style.css">
+</head>
+<body>
 
-function login(){
-  alert("LOGIN FROM temp_script.js");
-}
+<div class="card">
+  <h3 id="examTitle"></h3>
+  <h4 id="qText"></h4>
+  <div id="options"></div>
 
+  <button onclick="next()">Next</button>
+</div>
+
+<script src="temp_script.js"></script>
+</body>
+</html>
